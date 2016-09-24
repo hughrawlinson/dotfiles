6ea714c821ff53622635439ca3e777a10e744730
Hugh Rawlinson <hugh@spotify.com>
2016-09-24 16:55:03 +0200

    Big Vim Update

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,10 +2,10 @@
 
 git pull origin master;
 
-rsync --exclude-from "./.rsyncignore" -avh --no-perms . ~;
-
-# ln -sf $HOME/.dotfiles/.tmux.conf ~/.tmux.conf
-# ln -sf $HOME/.dotfiles/.vimrc ~/.vimrc
-# ln -sf $HOME/.dotfiles/.zshrc ~/.zshrc
-# ln -sf $HOME/.dotfiles/.vim ~/.vim
-# ln -sf $HOME/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -sf $HOME/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf $HOME/.dotfiles/vim ~/.vim
+ln -sf $HOME/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf $HOME/.dotfiles/.gitignore ~/.gitignore
+ln -sf $HOME/.dotfiles/.zshrc ~/.zshrc
+ln -sf $HOME/.dotfiles/.zshenv ~/.zshenv
+ln -sf $HOME/.dotfiles/.spacemacs ~/.spacemacs
