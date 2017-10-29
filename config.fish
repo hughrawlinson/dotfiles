fcf4b01729e98945237884cc54829b77cc930337
Hugh Rawlinson <hugh@spotify.com>
2017-10-29 13:23:28 +0100

    Update

---
+++ config.fish
@@ -0,0 +1,49 @@
+# export ZSH=$HOME/.oh-my-zsh
+
+# # Oh My ZSH
+# plugins=(
+#   brew
+#   common-aliases
+#   compleat
+#   dirpersist
+#   docker
+#   git
+#   git-fast
+#   git-extras
+#   last-working-dir
+#   npm
+#   nyan
+#   osx
+#   pyenv
+#   python
+#   ssh-agent
+#   sudo
+#   tmux
+#   tmuxinator
+#   )
+# source $ZSH/oh-my-zsh.sh
+
+# eval "(rbenv init -)"
+# if which pyenv > /dev/null then
+#   eval "(pyenv init -)"
+# end
+# [ -f /Users/hugh/.travis/travis.sh ]; and source /Users/hugh/.travis/travis.sh
+# source ~/.cargo/env
+# eval (opam config env)
+
+# alias tmux="tmux -2"
+# alias spm='npm --registry https://artifactory.spotify.net/artifactory/api/npm/virtual-npm --userconfig ~/.spmrc --always-auth=true'
+# alias ghci="stack ghci"
+# alias ghc="stack ghc"
+# alias npm-exec='PATH=(npm bin):$PATH'
+# #alias ec="emacsclient --no-wait -c"
+# #alias e="emacsclient -t"
+# alias em="emacsclient -t"
+# alias e="$EDITOR"
+# alias ds='docker-machine start default && docker-machine env default'
+
+# function loadenv
+#   cat .env | xargs export
+# end
+
+# autoload -U compinit; compinit
