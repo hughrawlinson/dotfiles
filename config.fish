cf9ce854e2104e8f42350274af2f193629839a35
Hugh Rawlinson <hugh@spotify.com>
2017-10-29 22:16:47 +0100

    Add fish config

--- config.fish
+++ config.fish
@@ -1,49 +1,9 @@
-# export ZSH=$HOME/.oh-my-zsh
+bass source ~/.nvm/nvm.sh
+source ~/.config/fish/functions/nvm.fish
+set PATH $PATH $HOME/.local/bin
 
-# # Oh My ZSH
-# plugins=(
-#   brew
-#   common-aliases
-#   compleat
-#   dirpersist
-#   docker
-#   git
-#   git-fast
-#   git-extras
-#   last-working-dir
-#   npm
-#   nyan
-#   osx
-#   pyenv
-#   python
-#   ssh-agent
-#   sudo
-#   tmux
-#   tmuxinator
-#   )
-# source $ZSH/oh-my-zsh.sh
+bass source '/Users/hugh/google-cloud-sdk/path.bash.inc'
+bass source '/Users/hugh/google-cloud-sdk/completion.bash.inc'
 
-# eval "(rbenv init -)"
-# if which pyenv > /dev/null then
-#   eval "(pyenv init -)"
-# end
-# [ -f /Users/hugh/.travis/travis.sh ]; and source /Users/hugh/.travis/travis.sh
-# source ~/.cargo/env
-# eval (opam config env)
+alias spm='npm --registry https://artifactory.spotify.net/artifactory/api/npm/virtual-npm --userconfig ~/.spmrc --always-auth=true'
 
-# alias tmux="tmux -2"
-# alias spm='npm --registry https://artifactory.spotify.net/artifactory/api/npm/virtual-npm --userconfig ~/.spmrc --always-auth=true'
-# alias ghci="stack ghci"
-# alias ghc="stack ghc"
-# alias npm-exec='PATH=(npm bin):$PATH'
-# #alias ec="emacsclient --no-wait -c"
-# #alias e="emacsclient -t"
-# alias em="emacsclient -t"
-# alias e="$EDITOR"
-# alias ds='docker-machine start default && docker-machine env default'
-
-# function loadenv
-#   cat .env | xargs export
-# end
-
-# autoload -U compinit; compinit
