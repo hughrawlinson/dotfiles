105afb26179f99ce49bf49654a94e93934964fc9
Hugh Rawlinson <hugh@spotify.com>
2017-11-21 14:18:53 +0100

    Stop warnings

--- config.fish
+++ config.fish
@@ -6,4 +6,5 @@ bass source '/Users/hugh/google-cloud-sdk/path.bash.inc'
 bass source '/Users/hugh/google-cloud-sdk/completion.bash.inc'
 
 alias spm='npm --registry https://artifactory.spotify.net/artifactory/api/npm/virtual-npm --userconfig ~/.spmrc --always-auth=true'
+alias tcp='tmux save-buffer - | pbcopy'
 
