1c72571b44fedf3b3b5f0616a060d31af387ccde
Hugh Rawlinson <hugh@spotify.com>
2017-11-26 22:04:01 +0100

    UPDATES

--- config.fish
+++ config.fish
@@ -2,8 +2,10 @@ bass source ~/.nvm/nvm.sh
 source ~/.config/fish/functions/nvm.fish
 set PATH $PATH $HOME/.local/bin
 
-bass source '/Users/hugh/google-cloud-sdk/path.bash.inc'
-bass source '/Users/hugh/google-cloud-sdk/completion.bash.inc'
+if test -d '/Users/hugh/google-cloud-sdk'
+  bass source '/Users/hugh/google-cloud-sdk/path.bash.inc'
+  bass source '/Users/hugh/google-cloud-sdk/completion.bash.inc'
+end
 
 alias spm='npm --registry https://artifactory.spotify.net/artifactory/api/npm/virtual-npm --userconfig ~/.spmrc --always-auth=true'
 alias tcp='tmux save-buffer - | pbcopy'
