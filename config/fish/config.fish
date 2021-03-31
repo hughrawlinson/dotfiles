48591902bea23a36463b1a873c4d4be6c75f6cda
Hugh Rawlinson <hughr2005@gmail.com>
2021-03-31 12:13:23 +0200

    periodic updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -36,79 +36,86 @@ fundle plugin 'tuvistavie/fish-ssh-agent'
 fundle plugin 'brigand/fast-nvm-fish'
 fundle init
 
-nvm use default >> /dev/null
+  nvm use default >> /dev/null
 
-# If we have linuxbrew installed, add to path
-if test -d "/home/linuxbrew"
-    set PATH /home/linuxbrew/.linuxbrew/bin $PATH
-end
+  # If we have linuxbrew installed, add to path
+  if test -d "/home/linuxbrew"
+      set PATH /home/linuxbrew/.linuxbrew/bin $PATH
+  end
 
-# If we have Google Cloud SDK installed on Mac, source
-if test -d '/Users/hugh/google-cloud-sdk'
-    bass source '/Users/hugh/google-cloud-sdk/path.bash.inc'
-    bass source '/Users/hugh/google-cloud-sdk/completion.bash.inc'
-end
+  # If we have Google Cloud SDK installed on Mac, source
+  if test -d '/Users/hugh/google-cloud-sdk'
+      bass source '/Users/hugh/google-cloud-sdk/path.bash.inc'
+      bass source '/Users/hugh/google-cloud-sdk/completion.bash.inc'
+  end
 
-if type -fq rbenv
-    status --is-interactive
-    and source (rbenv init -|psub)
-end
+  if test -d "$HOME/.rbenv"
+    set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+    # if type -fq rbenv
+        status --is-interactive
+        and source (rbenv init -|psub)
+    # end
+  end
 
-# If we have cargo installed, add to path for some rust dev
-if test -d "$HOME/.cargo/bin"
-    set -g PATH $HOME/.cargo/bin $PATH
-end
+  # If we have cargo installed, add to path for some rust dev
+  if test -d "$HOME/.cargo/bin"
+      set -g PATH $HOME/.cargo/bin $PATH
+  end
 
-# if type -f go;
-#     set -g PATH (go env GOPATH);
-# end;
+  # if type -f go;
+  #     set -g PATH (go env GOPATH);
+  # end;
 
-# If we have pyenv installed, add to path for python dev
-if test -d "$HOME/.pyenv"
-    set PYENV_ROOT "$HOME/.pyenv"
-    set PATH "$PYENV_ROOT/bin" $PATH
-    # https://github.com/pyenv/pyenv/issues/688
-    set -g -x GIT_INTERNAL_GETTEXT_TEST_FALLBACKS 1
-    if status --is-interactive
-        source (pyenv init -|psub)
-        source (pyenv virtualenv-init -|psub)
-    end
-end
+  # If we have pyenv installed, add to path for python dev
+  if test -d "$HOME/.pyenv"
+      set PYENV_ROOT "$HOME/.pyenv"
+      set PATH "$PYENV_ROOT/bin" $PATH
+      # https://github.com/pyenv/pyenv/issues/688
+      set -g -x GIT_INTERNAL_GETTEXT_TEST_FALLBACKS 1
+      if status --is-interactive
+          source (pyenv init -|psub)
+          source (pyenv virtualenv-init -|psub)
+      end
+  end
 
-# Required for GPG signing
-set -gx GPG_TTY (tty)
+  # Required for GPG signing
+  set -gx GPG_TTY (tty)
 
-if status --is-interactive
-    bind -k f4 edit_cmd; commandline -f execute
-    # if setxkbmap, swap caps and esc
-    if test "Darwin" != (uname -a | cut -d' ' -f1)
-    #     if type -q setxkbmap
-    #         setxkbmap -option caps:swapescape
-    #     end
-    else
-      defaults write -g ApplePressAndHoldEnabled -bool false
-    end
-    # Keybinding to refresh fish config
-    bind -k f5 eval "source $HOME/.config/fish/config.fish"
-end
+  if status --is-interactive
+      bind -k f4 edit_cmd; commandline -f execute
+      # if setxkbmap, swap caps and esc
+      if test "Darwin" != (uname -a | cut -d' ' -f1)
+      #     if type -q setxkbmap
+      #         setxkbmap -option caps:swapescape
+      #     end
+      else
+        defaults write -g ApplePressAndHoldEnabled -bool false
+      end
+      # Keybinding to refresh fish config
+      bind -k f5 eval "source $HOME/.config/fish/config.fish"
+  end
 
-# Make sure to have user scripts in path
-if not contains "$HOME/.local/bin" $PATH
-    set PATH "$HOME/.local/bin" $PATH
-end
+  # Make sure to have user scripts in path
+  if not contains "$HOME/.local/bin" $PATH
+      set PATH "$HOME/.local/bin" $PATH
+  end
 
-if test -f /usr/libexec/java_home
-    set JAVA_VERSION "1.8"
-    set JAVA_HOME (/usr/libexec/java_home -v $JAVA_VERSION)
-    set JAVACMD "$JAVA_HOME/bin/java"
+  if test -f /usr/libexec/java_home
+      set JAVA_VERSION "1.8"
+      set JAVA_HOME (/usr/libexec/java_home -v $JAVA_VERSION)
+      set JAVACMD "$JAVA_HOME/bin/java"
 
-    set PATH $HOME/.jenv/bin $PATH
-    status --is-interactive; and source (jenv init -|psub)
-end
+      set PATH $HOME/.jenv/bin $PATH
+      status --is-interactive; and source (jenv init -|psub)
+  end
 
-set -x GPG_TTY (tty)
-set PATH "/usr/local/sbin" $PATH
+  set -x GPG_TTY (tty)
+  set PATH "/usr/local/sbin" $PATH
+  set DISPLAY "localhost:0"
 
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/hugh/google-cloud-sdk/path.fish.inc' ]; . '/home/hugh/google-cloud-sdk/path.fish.inc'; end
+  # The next line updates PATH for the Google Cloud SDK.
+  if [ -f '/Users/hugh/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/hugh/Downloads/google-cloud-sdk/path.fish.inc'; end
+
+  alias emsdk_setup "source $HOME/emsdk/emsdk_env.fish"
+
