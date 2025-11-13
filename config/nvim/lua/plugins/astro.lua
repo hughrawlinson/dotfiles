87e53a6e185dc99cc7569a801d369fe72ae19b14
Hugh Rawlinson <hughr2005@gmail.com>
2025-11-14 00:28:28 +0100

    add lazyvim

--- config/nvim/lua/plugins/astro.lua
+++ config/nvim/lua/plugins/astro.lua
@@ -3,10 +3,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, { "astro" })
-    end
+    end,
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, { "astro-language-server" })
     end,
